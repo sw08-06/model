1e227124e764bb9da1ba70f5a91129ddf7f0cf70
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2024-04-02 15:20:41 +0200

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,7 +3,7 @@ import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 
 class FramePreprocessor:
-    def __init__(self, data_path, data_types, output_path, max_workers=None):
+    def __init__(self, data_path, data_types):
         """
         Initializes a FramePreprocessor instance.
         Args: data_path (str): The path to the directory containing the data.
@@ -13,58 +13,31 @@ class FramePreprocessor:
         """
         self.data_path = data_path
         self.data_types = data_types
-        self.output_path = output_path
-        self.max_workers = max_workers
 
-    def load_frames(self, subject):
+    def preprocess_frames(self, subject):
         """
         Loads frames from the specified subject directory.
         Args: subject: Name of the subject.
         Returns: dict - A dictionary containing loaded frames, organized by label and index.
         """
-        frames = {}
-        subject_path = os.path.join(self.data_path, subject)
-
         for data_type in self.data_types:
             print("loading datatype: " + data_type)
-            subject_signal_path = os.path.join(subject_path, data_type)
-            for file_name in os.listdir(subject_signal_path):
-                if file_name.endswith(".npy"):
-                    label = int(file_name.split("_")[0])
-                    index = int(file_name.split("_")[-1].split(".")[0])
-                    file_path = os.path.join(subject_signal_path, file_name)
-                    if (label, index) not in frames:
-                        frames[(label, index)] = {}
-                    frames[(label, index)][data_type] = np.load(file_path)
-        
-        return frames
-    
-    def detect_outliers(self, feature_vector):
-        pass
-        
-    def normalize_feature_vector(self, feature_vector):
-        pass
-    
-    def process_subject(self, subject):
-        """
-        Preprocesses frames for a specific subject and saves the feature vectors.
-        Args: subject: Name of the subject.
-        """
-        frames = self.load_frames(subject)
-        
-        print("all frames loaded.")
-        output_subject_dir = os.path.join("data", self.output_path, subject)
-        os.makedirs(output_subject_dir, exist_ok=True)
-        
-        for (label, index), data in frames.items():
-            feature_vector = np.array([(data[data_type]) for data_type in self.data_types])
-            # TODO: uncomment detect_outliers and fill out
-            #feature_vector = self.detect_outliers(feature_vector)
-            # TODO: 
-            #feature_vector = self.normalize_feature_vector(feature_vector)
+            subject_signal_path = os.path.join(self.data_path, subject, data_type)
+            output_dir = os.path.join("data", "preprocessed_frames", subject, data_type)
+            os.makedirs(output_dir, exist_ok=True)
             
-            output_file_path = os.path.join(output_subject_dir, f"{label}_FV_{index}.npy")
-            np.save(output_file_path, feature_vector)
+            for file_name in os.listdir(subject_signal_path):
+                file_path = os.path.join(subject_signal_path, file_name)
+                frame = np.load(file_path)
+                frame = self._detect_outliers(frame)
+                frame = self._normalize_frame(frame)
+                np.save(os.path.join(output_dir, file_name.split(".")[0], "_preprocessed.npy"), frame)
+    
+    def _detect_outliers(self, frame):
+        pass
+        
+    def _normalize_frame(self, frame):
+        pass
 
     def process_all_subjects(self):
         """
@@ -72,17 +45,16 @@ class FramePreprocessor:
         """
         subjects = [subject for subject in os.listdir(self.data_path)]
 
-        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+        with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
             for subject in subjects:
-                executor.submit(self.process_subject, subject)
+                executor.submit(self.preprocess_frames, subject)
 
 
 if __name__ == "__main__":
     preprocessor = FramePreprocessor(
         data_path=os.path.join("data", "frames"), 
         data_types=["BVP", "EDA", "TEMP"],
-        output_path="preprocessed_frames", 
-        max_workers=os.cpu_count())
+    )
 
     os.makedirs(os.path.join("data", "preprocessed_frames"), exist_ok=True)
 
