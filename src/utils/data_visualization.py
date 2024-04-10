f8c9c77755e7745a849cda4d8910d277c72a1ecf
Lasse Andersen <lran18@student.aau.dk>
2024-04-10 12:55:56 +0200

    update to data visualization

--- src/utils/data_visualization.py
+++ src/utils/data_visualization.py
@@ -2,35 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def visualize_frames(file_paths):
+def visualize_frames(file_path):
     """
     Visualizes frames from the given files in subplots.
     Args: file_paths: Paths to the files containing the frame data.
     """
-    _, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
+    _, axes = plt.subplots(nrows=3, ncols=1, figsize=(12, 8))
 
-    for i, file_path in enumerate(file_paths):
-        frame = np.load(file_path)
+    frames = np.load(file_path, allow_pickle=True)
 
-        ax = axes[i // 2, i % 2]
+    for i, frame in enumerate(frames):
+        ax = axes[i]
         ax.plot(frame)
         ax.set_title(f"Frame {i+1}")
         ax.set_xlabel("Sample Index")
         ax.set_ylabel("Value")
         ax.grid(True)
         ax.set_xlim(0, len(frame))
-        ax.set_ylim(frame.min(), frame.max())
+        ax.set_ylim(np.min(frame), np.max(frame))
 
     plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
-    paths = [
-        "data/frames/S2/BVP/0_BVP_10.npy",
-        "data/frames/S5/BVP/0_BVP_0.npy",
-        "data/preprocessed_frames/S2/BVP/0_BVP_10_preprocessed.npy",
-        "data/preprocessed_frames/S5/BVP/0_BVP_0_preprocessed.npy",
-    ]
-
-    visualize_frames(paths)
+    visualize_frames("data/frames/validation/S16/1_7.npy")
