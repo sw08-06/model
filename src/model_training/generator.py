24e692554b1d341df5d95fc319f0a75a6b47a1f5
lasse2507a <lran18@student.aau.dk>
2024-04-13 13:32:12 +0200

    updated generator to fit h5

--- src/model_training/generator.py
+++ src/model_training/generator.py
@@ -1,31 +1,32 @@
-import os
+import random
 import numpy as np
 import keras
+import h5py
 
 
-class Generator(keras.utils.PyDataset):
+class Generator:
     def __init__(self, path, batch_size):
         self.path = path
         self.batch_size = batch_size
-        self.subjects = [subject for subject in os.listdir(self.path)]
-        files = []
-        for subject in self.subjects:
-            files += [os.path.join(self.path, subject, file) for file in os.listdir(os.path.join(self.path, subject))]
-        self.files = files
-        self.num_files = len(self.files)
+        self.data = h5py.File(self.path, "r")
+        self.data_type_groups = list(self.data.keys())
+        self.first_dataset_names = list(self.data[self.data_type_groups[0]].keys())
+        self.num_datasets = len(self.first_dataset_names)
+        self.random_indices = [i for i in range(self.num_datasets)]
+        random.shuffle(self.random_indices)
 
     def __len__(self):
-        return int(np.floor(self.num_files / self.batch_size))
+        return int(np.floor(self.num_datasets / self.batch_size))
 
     def __getitem__(self, idx):
-        batch_files = self.files[idx * self.batch_size:(idx + 1) * self.batch_size]
-        batch_data = []
-        batch_labels = []
-        for file in batch_files:
-            file_data = np.load(file)
-            batch_data.append(file_data)
-            label = int(os.path.basename(file).split("_")[0])
-            batch_labels.append(label)
-        batch_data = np.array(batch_data)
-        batch_labels = np.array(batch_labels)
+        batch_indices = self.random_indices[idx * self.batch_size : (idx + 1) * self.batch_size]
+        batch_data = [[] for _ in self.data_type_groups]
+        batch_labels = [[] for _ in self.data_type_groups]
+        for i, data_type in enumerate(self.data_type_groups):
+            dataset_names = list(self.data[data_type].keys())
+            for j in batch_indices:
+                batch_data[i].append(self.data[data_type][dataset_names[j]][:].flatten())
+                batch_labels[i].append(self.data[data_type][dataset_names[j]].attrs["label"])
+            batch_data[i] = np.array(batch_data[i])
+            batch_labels[i] = np.array(batch_labels[i])
         return batch_data, batch_labels
