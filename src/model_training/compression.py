063c01945c3326a62e3367b21f1388ab97515fb9
lasse2507a <lran18@student.aau.dk>
2024-05-28 18:52:00 +0200

    model compression

--- src/model_training/compression.py
+++ src/model_training/compression.py
@@ -1,6 +1,8 @@
 import os
 import tensorflow as tf
-import keras
+from tensorflow import lite, io
+from keras.models import load_model
+from architectures import SliceLayer
 
 
 def compress_model(model_name):
@@ -11,12 +13,20 @@ def compress_model(model_name):
     Args:
         model_name (str): Name of the model to compress
     """
-    model = keras.models.load_model(f"models/{model_name}.keras")
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    tf_flite_model = converter.convert()
+    model = load_model(f"models/{model_name}.keras", custom_objects={"SliceLayer": SliceLayer})
+
+    @tf.function
+    def wrapped_model(input_tensor):
+        return model(input_tensor)
+
+    concrete_func = wrapped_model.get_concrete_function(tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
+
+    converter = lite.TFLiteConverter.from_concrete_functions([concrete_func])
+    converter.optimizations = [lite.Optimize.DEFAULT]
+    tf_lite_model = converter.convert()
+
     os.makedirs("models_compressed", exist_ok=True)
-    tf.io.write_file(f"models_compressed/{model_name}.tflite", tf_flite_model)
+    io.write_file(f"models_compressed/{model_name}.tflite", tf_lite_model)
 
 
 if __name__ == "__main__":
